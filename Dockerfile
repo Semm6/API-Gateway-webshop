a1537f9a3c1b26c5424e51998aadff6ca5463d18
Semm6 <sem.dewilde6@gmail.com>
2021-10-13 15:49:29 +0200

    Added docker and docker compose file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:11
-EXPOSE 9191
+EXPOSE 9192
 ADD target/API-Gateway-webshop.jar API-Gateway-webshop.jar
 ENTRYPOINT ["java", "-jar", "/API-Gateway-webshop.jar"]
--- src/test/java/com/example/apigatewaywebshop/ApiGatewayWebshopApplicationTests.java
+++ src/test/java/com/example/apigatewaywebshop/ApiGatewayWebshopApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.apigatewaywebshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ApiGatewayWebshopApplicationTests {
 
     @Test
